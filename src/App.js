12d4bcc316e61476e0a2a82256f1c9c07d79eca3
Caroline <wanguic49@gmail.com>
2024-11-12 12:56:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import  { useState } from 'react';
 
-function App() {
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function LoginForm({ onLogin }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <h2>Login</h2>
+          <button onClick={onLogin}>Log In</button>
+      </div>
   );
 }
 
-export default App;
+function AdminPanel({ onLogout }) {
+  return (
+      <div>
+          <h2>Admin Panel</h2>
+          <p>Welcome to the admin panel!</p>
+          <button onClick={onLogout}>Log Out</button>
+      </div>
+  );
+}
+
+
+
+
+export default function Profile() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  let content;
+    if (isLoggedIn) {
+        content = <AdminPanel onLogout={() => setIsLoggedIn(false)} />;
+    } else {
+        content = <LoginForm onLogin={() => setIsLoggedIn(true)} />;
+    }
+
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
+
+      <div>
+        {content}
+      </div>
+    </>
+  );
+}
+
+
